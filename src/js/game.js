220b571a3fd1a8c66246f98bf13ac05e93e0c18c
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-10-26 00:55:50 +0300

    continue work, mb delete /node_modules/ from git

---
+++ src/js/game.js
@@ -0,0 +1,19 @@
+class Game {
+    start() {
+        console.log('game started');
+    };
+}
+
+class GameSavingData {
+
+}
+
+
+function readGameSaving () {
+
+}
+
+
+function writeGameSaving () {
+
+}
